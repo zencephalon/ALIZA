0a3b6516ed7ccbf466d1326a5b91d5b3ef2291b6
Matthew Bunday <zenceph@gmail.com>
2013-03-06 15:43:38 -0500

    some incremental work, I think I have to think a bit more about how this architecture will work.

--- spine.rb
+++ spine.rb
@@ -1,8 +1,21 @@
 class Spine
-    attr_accessor :current_state, :states, :transitions, :events
+    attr_accessor :state, :states, :transitions, :events, :event
+
+    def initialize
+        @state = :new
+        @states = [:new, :ask_name]
+        @events = [:initialize]
+        @event = nil
+
+        @transitions = [Transition.new(:initialize, 'A new Aliza is created.', :initialize, :ask_name)]
+
+    end
 
 
     def execute
+        if @event.nil?
+            self.receive_event(:initialize)
+        end
 
     end
 
